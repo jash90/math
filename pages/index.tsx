e143d3b0e3572f380db937e56c155efcf0f796c4
Bartlomiej Zimny <b.zimny@getprintbox.com>
2020-12-30 15:20:58 +0100

    change level

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
 import Layout from '../components/Layout'
 import { Form, Result, Text } from '../components/StyledComponents';
 import Lvl from '../components/Lvl';
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useMemo, useRef, useState } from 'react'
 import { generate } from '../mechanism/multiply';
 import { GameLevel } from '../interfaces/enum';
 
 
 
 const IndexPage = () => {
+  const input = useRef(null);
   const [points, setPoints] = useState(0);
-  const [level, setLevel] = useState(GameLevel.MID);
+  const [level, setLevel] = useState(GameLevel.LOW);
 
   const generateLvl = useCallback(() => generate(level), [level]);
 
   const [state, setState] = useState(generateLvl);
 
-  const onKeyPress = useCallback(
-    (e: any) => {
-      if (e.key === 'Enter' && e.target.value !== "") {
-        if (e.target.value == state.result) {
-          setState(generateLvl);
-          setPoints(points + 1);
-          e.target.value = "";
-          switch (points) {
-            case GameLevel.MID: setLevel(GameLevel.MID); break;
-            case GameLevel.HIGH: setLevel(GameLevel.HIGH); break;
-          }
-        } else {
-          if (points < 50) setPoints(points - 1)
-          if (points > 50 && points < 100) setPoints(points - 2)
-          if (points > 100) setPoints(points - 5)
+  const onKeyPress = (e: any) => {
+    if (e.key === 'Enter' && input.current.value !== "") {
+      if (e.target.value == state.result) {
+        setState(generateLvl);
+        setPoints(points + 1);
+        e.target.value = "";
+        switch (points) {
+          case GameLevel.MID: setLevel(GameLevel.MID); break;
+          case GameLevel.HIGH: setLevel(GameLevel.HIGH); break;
         }
+      } else {
+        if (points < 50) setPoints(points - 1)
+        if (points > 50 && points < 100) setPoints(points - 2)
+        if (points > 100) setPoints(points - 5)
       }
-    }, []);
+    }
+  };
 
   const Level = useMemo(() => (<Lvl level={level} />), [level])
 
@@ -44,7 +44,7 @@ const IndexPage = () => {
         <Text>{"∙"}</Text>
         <Text>{state.factor2}</Text>
         <Text>{"="}</Text>
-        <Result onKeyPress={onKeyPress} />
+        <Result ref={input} onKeyPress={onKeyPress} />
       </Form>
       <Form><Text>{"Punkty : " + points}</Text></Form>
       {Level}
